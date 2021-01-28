6c6d10e81a83dc3e2a394fc563d5c1a2f8b89ba6
Andrea Tagliabue <tagliabue.andrea5@gmail.com>
2021-01-28 11:53:47 +0100

    Exercise changes

--- src/main/java/mex/s04/Exercise1.java
+++ src/main/java/mex/s04/Exercise1.java
@@ -10,7 +10,7 @@ public class Exercise1 {
      */
     public static long sum(int first, int last) {
         long result = 0;
-        // TODO
+        // TODO: still thinking about what to do actually... :D
         return result;
     }
 }
